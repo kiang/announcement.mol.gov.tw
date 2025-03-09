43ebeb870f16e749de6cc0d4512d3f2cc9105265
kiang <kiange@gmail.com>
2025-03-09 10:03:50 +0800

    keep all records as the source may drop them

---
+++ scripts/00_build_base.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * extract all commits using command
+ * ```git rev-list --all | xargs -L1 sh -c 'mkdir "/home/kiang/public_html/mol-rev/$0" && git archive --format=tar $0 | tar -C "/home/kiang/public_html/mol-rev/$0" -xf -'```
+ */
+$fh = fopen(__DIR__ . '/csvfiles', 'r');
+$basePath = dirname(__DIR__);
+$baseHead = ['主管機關', '公告日期', '處分日期', '處分字號', '事業單位名稱或負責人', '違法法規法條', '違反法規內容', '罰鍰金額', '備註說明'];
+while ($line = fgets($fh)) {
+    $csvFile = trim($line);
+    if ('text/csv' !== mime_content_type($csvFile)) {
+        continue;
+    }
+    $csvFh = fopen($csvFile, 'r');
+    $head = fgetcsv($csvFh, 2048);
+    $countHead = count($head);
+    while ($csvLine = fgetcsv($csvFh, 20000)) {
+        if ($countHead !== count($csvLine)) {
+            continue;
+        }
+        $data = array_combine($head, $csvLine);
+        if (!isset($data['罰鍰金額'])) {
+            $data['罰鍰金額'] = '';
+        }
+        if (isset($data['處分金額或滯納金'])) {
+            $data['罰鍰金額'] = $data['處分金額或滯納金'];
+        }
+        $finalLine = [];
+        foreach ($baseHead as $key) {
+            $finalLine[$key] = $data[$key];
+        }
+        $year = substr($finalLine['處分日期'], 0, 4);
+        $dataPath = "{$basePath}/data/{$year}/{$finalLine['主管機關']}";
+        if (!file_exists($dataPath)) {
+            mkdir($dataPath, 0777, true);
+        }
+        $pos = strpos($finalLine['處分字號'], '號');
+        if(false !== $pos) {
+            $finalLine['處分字號'] = substr($finalLine['處分字號'], 0, $pos + 3);
+        }
+        file_put_contents("{$dataPath}/{$finalLine['處分字號']}.json", json_encode($finalLine, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
+    }
+}
---
+++ scripts/01_fetch.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * api reference
+ * https://apiservice.mol.gov.tw/OdService/openapi/OAS.html
+ * 
+ * metadata
+ * 勞動基準法 A17000000J-030225-svj
+ * https://apiservice.mol.gov.tw/OdService/rest/dataset/109896
+ * 就業服務法 A17000000J-030228-p2G
+ * https://apiservice.mol.gov.tw/OdService/rest/dataset/110908
+ * 性別工作平等法 A17000000J-030226-sop
+ * https://apiservice.mol.gov.tw/OdService/rest/dataset/109897
+ * 中高齡者及高齡者就業促進法 A17000000J-030472-iw5
+ * https://apiservice.mol.gov.tw/OdService/rest/dataset/156904
+ * 工會法 A17000000J-030542-tEK
+ * https://apiservice.mol.gov.tw/OdService/rest/dataset/166670
+ * 
+ * 違反勞動法令事業單位
+ */
+
+$context = stream_context_create(["ssl" => array(
+    "verify_peer" => false,
+    "verify_peer_name" => false,
+)]);
+$basePath = dirname(__DIR__);
+
+$resources = [
+    'A17000000J-030225-svj' => '勞動基準法',
+    'A17000000J-030228-p2G' => '就業服務法',
+    'A17000000J-030226-sop' => '性別工作平等法',
+    'A17000000J-030472-iw5' => '中高齡者及高齡者就業促進法',
+    'A17000000J-030542-tEK' => '工會法',
+];
+
+$baseHead = ['主管機關', '公告日期', '處分日期', '處分字號', '事業單位名稱或負責人', '違法法規法條', '違反法規內容', '罰鍰金額', '備註說明'];
+
+foreach($resources AS $resourceId => $resourceName) {
+    $fh = fopen('https://apiservice.mol.gov.tw/OdService/download/' . $resourceId, 'r', false, $context);
+    $head = fgetcsv($fh, 2048);
+    while ($line = fgetcsv($fh, 8000)) {
+        $data = array_combine($head, $line);
+        if (!isset($data['罰鍰金額'])) {
+            $data['罰鍰金額'] = '';
+        }
+        if (isset($data['處分金額或滯納金'])) {
+            $data['罰鍰金額'] = $data['處分金額或滯納金'];
+        }
+        $finalLine = [];
+        foreach ($baseHead as $key) {
+            $finalLine[$key] = $data[$key];
+        }
+        $year = substr($finalLine['處分日期'], 0, 4);
+        $dataPath = "{$basePath}/data/{$year}/{$finalLine['主管機關']}";
+        if (!file_exists($dataPath)) {
+            mkdir($dataPath, 0777, true);
+        }
+        $pos = strpos($finalLine['處分字號'], '號');
+        if(false !== $pos) {
+            $finalLine['處分字號'] = substr($finalLine['處分字號'], 0, $pos + 3);
+        }
+        file_put_contents("{$dataPath}/{$finalLine['處分字號']}.json", json_encode($finalLine, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT));
+    }
+}
