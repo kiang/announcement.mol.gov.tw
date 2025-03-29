a8a1146913b075a1ba7c3e7cc163e90ba9c42ef5
kiang <kiange@gmail.com>
2025-03-29 14:57:33 +0800

    prevent unexpected characters in filename

--- scripts/02_merge_by_company.php
+++ scripts/02_merge_by_company.php
@@ -5,37 +5,62 @@ if (!is_dir($companyPath)) {
     mkdir($companyPath, 0777, true);
 }
 
+$tr = [
+    '\u0000' => '',
+    '(' => '',
+    ')' => '',
+    '（' => '',
+    '）' => '',
+    '/' => '',
+    '、' => '',
+    chr(0) => '',
+    '?' => '',
+    '_' => '',
+    '-' => '',
+    '：' => '',
+    '�' => '',
+    '' => '',
+];
 foreach (glob('data/*/*/*.json') as $file) {
     $data = json_decode(file_get_contents($file), true);
+    $codePointPos = strpos($data['事業單位名稱或負責人'], '&#');
+    while(false !== $codePointPos) {
+        $codePointEnd = strpos($data['事業單位名稱或負責人'], ';', $codePointPos);
+        if (false === $codePointEnd) {
+            break;
+        }
+        $codePoint = substr($data['事業單位名稱或負責人'], $codePointPos, $codePointEnd - $codePointPos + 1);
+        $codePointResult = html_entity_decode($codePoint);
+        if($codePointResult === $codePoint) {
+            $codePointResult = '';
+        }
+        $data['事業單位名稱或負責人'] = str_replace($codePoint, $codePointResult, $data['事業單位名稱或負責人']);
+        $codePointPos = strpos($data['事業單位名稱或負責人'], '&#');
+    }
+    $data['事業單位名稱或負責人'] = strtr($data['事業單位名稱或負責人'], $tr);
     $pos = strpos($data['事業單位名稱或負責人'], '有限公司');
+    $company = '';
     if ($pos !== false) {
         $company = substr($data['事業單位名稱或負責人'], 0, $pos + 12);
-        $company = preg_replace('/[\\(\\)\\/]+/', '', $company);
-        $company = preg_replace('/&#(\d+);/', '', $company);
-        $targetFile = $companyPath . '/' . $company . '.csv';
-        if (!file_exists($targetFile)) {
-            $fh = fopen($targetFile, 'w');
-            fputcsv($fh, array_keys($data));
-            fclose($fh);
-        }
-        $fh = fopen($targetFile, 'a');
-        fputcsv($fh, $data);
-        fclose($fh);
     } else {
         $parts = explode('即', $data['事業單位名稱或負責人']);
         if (count($parts) === 2) {
             $company = $parts[1];
-            $company = preg_replace('/[\\(\\)\\/]+/', '', $company);
-            $company = preg_replace('/&#(\d+);/', '', $company);
-            $targetFile = $companyPath . '/' . $company . '.csv';
-            if (!file_exists($targetFile)) {
-                $fh = fopen($targetFile, 'w');
-                fputcsv($fh, array_keys($data));
-                fclose($fh);
-            }
-            $fh = fopen($targetFile, 'a');
-            fputcsv($fh, $data);
-            fclose($fh);
         }
     }
+    if (empty($company)) {
+        continue;
+    }
+    if(false !== strpos($company, chr(0))) {
+        die('error: ' . $company);
+    }
+    $targetFile = $companyPath . '/' . $company . '.csv';
+    if (!file_exists($targetFile)) {
+        $fh = fopen($targetFile, 'w');
+        fputcsv($fh, array_keys($data));
+        fclose($fh);
+    }
+    $fh = fopen($targetFile, 'a');
+    fputcsv($fh, $data);
+    fclose($fh);
 }
